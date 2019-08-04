756698eb8c4117a98272e82c5023f9000fb40ecb
pranav kumar <kumaarpranv@gmail.com>
2019-08-04 12:57:31 +0530

    yes12

--- main.py
+++ main.py
@@ -20,42 +20,192 @@ def set_servo_pulse(channel, pulse):
     pwm.set_pwm(channel, 0, pulse)
 
 def init():
-    print("init")
     #bl
     pwm.set_pwm(4,0,servo_max)
     time.sleep(0.3)
-
-    pwm.set_pwm(7,0,servo_min)
+    pwm.set_pwm(7,0,servo_min-40)
+    time.sleep(0.3)
+    pwm.set_pwm(5,0,servo_max-300)
     time.sleep(0.3)
     
-    pwm.set_pwm(5,0,servo_min1)
+    #br
+    pwm.set_pwm(10,0,servo_min+20)
     time.sleep(0.3)
-    
-
-    #br 
-    pwm.set_pwm(10,0,servo_min)
+    pwm.set_pwm(8,0,servo_max+20)
     time.sleep(0.3)
-
-    pwm.set_pwm(8,0,servo_max-40)
-    time.sleep(0.3)    
-    
-    pwm.set_pwm(11,0,servo_min1-25)
+    pwm.set_pwm(11,0,servo_min+50)
+    time.sleep(0.3)
+        
+    ##mid
+    pwm.set_pwm(2,0,servo_max-40)
     time.sleep(0.3)
 
-    #ml
-    pwm.set_pwm(2,0,servo_max)
+    pwm.set_pwm(3,0,servo_min-40)
     time.sleep(0.3)
-    pwm.set_pwm(3,0,servo_min)
-    time.sleep(0.3)
- 
-    #mr
+
     pwm.set_pwm(12,0,servo_min)
     time.sleep(0.3)
-    pwm.set_pwm(13,0,servo_max-40)
+
+    pwm.set_pwm(13,0,servo_max)
+    time.sleep(0.3)
+        
+    #tl
+    pwm.set_pwm(0,0,servo_max-20)
+    time.sleep(0.3)
+    pwm.set_pwm(6,0,servo_min-20)
+    time.sleep(0.3)
+    pwm.set_pwm(1,0,servo_min+100)
     time.sleep(0.3)
     
-    #tl
-    pwm.set_pwm(0,0,servo_max+40)
+    #tr
+    pwm.set_pwm(14,0,servo_min)
+    time.sleep(0.3)
+    pwm.set_pwm(9,0,servo_max)
+    time.sleep(0.3)
+    pwm.set_pwm(15,0,servo_max-150)
+    time.sleep(0.3)
+    
+
+def left():
+    #bls1
+    pwm.set_pwm(4,0,servo_max-75)
+    time.sleep(0.3)
+    #pwm.set_pwm(7,0,servo_min+25)
+    #time.sleep(0.3)
+    pwm.set_pwm(5,0,servo_max-375)
+    time.sleep(0.3)
+    
+    #bls2
+    pwm.set_pwm(4,0,servo_max)
+    time.sleep(0.3)
+    #pwm.set_pwm(7,0,servo_min-20)
+    #time.sleep(0.3)
+    pwm.set_pwm(5,0,servo_max-300)
+    time.sleep(0.3)
+    
+    #tls1
+    pwm.set_pwm(0,0,servo_max-95)
+    time.sleep(0.3)
+    #pwm.set_pwm(6,0,servo_min+25)
+    #time.sleep(0.3)
+    pwm.set_pwm(1,0,servo_min+25)
+    time.sleep(0.3)
+    
+    #tls2
+    pwm.set_pwm(0,0,servo_max-20)
+    time.sleep(0.3)
+    #pwm.set_pwm(6,0,servo_min-20)
+    #time.sleep(0.3)
+    pwm.set_pwm(1,0,servo_min+100)
     time.sleep(0.3)
 
-    pwm.set_pwm(6,0,servo_min+20)
+def right():
+    #brs1
+    pwm.set_pwm(10,0,servo_min+95)
+    time.sleep(0.3)
+    #pwm.set_pwm(8,0,servo_max+20)
+    #time.sleep(0.3)
+    pwm.set_pwm(11,0,servo_min+125)
+    time.sleep(0.3)
+
+    #brs2
+    pwm.set_pwm(10,0,servo_min+20)
+    time.sleep(0.3)
+    #pwm.set_pwm(8,0,servo_max+20)
+    #time.sleep(0.3)
+    pwm.set_pwm(11,0,servo_min+50)
+    time.sleep(0.3)
+    
+    #trs1
+    pwm.set_pwm(14,0,servo_min+75)
+    time.sleep(0.3)
+    #pwm.set_pwm(9,0,servo_max)
+    #time.sleep(0.3)
+    pwm.set_pwm(15,0,servo_max-75)
+    time.sleep(0.3)
+    
+    #trs2
+    pwm.set_pwm(14,0,servo_min)
+    time.sleep(0.3)
+    #pwm.set_pwm(9,0,servo_max)
+    #time.sleep(0.3)
+    pwm.set_pwm(15,0,servo_max-150)
+    time.sleep(0.3)
+
+
+def frwd():
+    #bls1
+    pwm.set_pwm(4,0,servo_max-55)
+    time.sleep(0.3)
+    #pwm.set_pwm(7,0,servo_min+25)
+    #time.sleep(0.3)
+    pwm.set_pwm(5,0,servo_max-375)
+    time.sleep(0.3)
+    
+    #bls2
+    pwm.set_pwm(4,0,servo_max+20)
+    time.sleep(0.3)
+    #pwm.set_pwm(7,0,servo_min-20)
+    #time.sleep(0.3)
+    pwm.set_pwm(5,0,servo_max-300)
+    time.sleep(0.3)
+
+    #brs1
+    pwm.set_pwm(10,0,servo_min+95)
+    time.sleep(0.3)
+    #pwm.set_pwm(8,0,servo_max+20)
+    #time.sleep(0.3)
+    pwm.set_pwm(11,0,servo_min+125)
+    time.sleep(0.3)
+
+    #brs2
+    pwm.set_pwm(10,0,servo_min+20)
+    time.sleep(0.3)
+    #pwm.set_pwm(8,0,servo_max+20)
+    #time.sleep(0.3)
+    pwm.set_pwm(11,0,servo_min+50)
+    time.sleep(0.3)
+
+    #tls1
+    pwm.set_pwm(0,0,servo_max-95)
+    time.sleep(0.3)
+    #pwm.set_pwm(6,0,servo_min+25)
+    #time.sleep(0.3)
+    pwm.set_pwm(1,0,servo_min+25)
+    time.sleep(0.3)
+    
+    #tls2
+    pwm.set_pwm(0,0,servo_max-20)
+    time.sleep(0.3)
+    #pwm.set_pwm(6,0,servo_min-20)
+    #time.sleep(0.3)
+    pwm.set_pwm(1,0,servo_min+100)
+    time.sleep(0.3)
+
+    #trs1
+    pwm.set_pwm(14,0,servo_min+75)
+    time.sleep(0.3)
+    #pwm.set_pwm(9,0,servo_max)
+    #time.sleep(0.3)
+    pwm.set_pwm(15,0,servo_max-75)
+    time.sleep(0.3)
+    
+    #trs2
+    pwm.set_pwm(14,0,servo_min)
+    time.sleep(0.3)
+    #pwm.set_pwm(9,0,servo_max)
+    #time.sleep(0.3)
+    pwm.set_pwm(15,0,servo_max-150)
+    time.sleep(0.3)
+
+
+pwm.set_pwm_freq(60)
+init()
+while True:
+    #pwm.set_pwm(9,0,servo_max)
+    #time.sleep(0.3)
+    #pwm.set_pwm(9,0,servo_max1)
+    #frwd()
+    #right()
+    #left()
+    pass
--- run.py
+++ run.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
+from main import *
+import json
 
+ct=0
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/get', methods=['GET'])
+def post():
+    mov=""
+    if request.method == "GET":
+        mov = request.args.get('mov')
+        #print(mov)
+        
+        if mov == "f":
+            frwd()
+        if mov == "l":
+            left()
+        if mov == "r":
+            right()
+        
+        return jsonify({"success" : "true"});
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',debug=True, threaded= True)
